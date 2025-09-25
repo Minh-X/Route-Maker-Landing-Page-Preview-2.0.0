898f9969c000f61efd29237e000b0bc52451dc63
mingthedev <tuilajboy@gmail.com>
2025-09-25 09:37:46 +0700

    littleChanging

--- style/index/index.js
+++ style/index/index.js
@@ -3,7 +3,7 @@ const downCount = document.querySelector("#downloadCount");
 document.addEventListener("DOMContentLoaded", function (e) {
   userCount();
   downloadCount();
-  imageLoadingSystem()
+  imageLoadingSystem();
 });
 function userCount() {
   let i = 4000;
@@ -12,49 +12,80 @@ function userCount() {
     useCount.textContent = `${i}`;
     if (i > 10000) {
       clearInterval(count);
-      useCount.textContent = "10,000+"
+      useCount.textContent = "10,000+";
     }
   }, 50);
 }
 function downloadCount() {
-let i = 950000;
+  let i = 950000;
   let count = setInterval(function () {
     i++;
     downCount.textContent = `${i}`;
     if (i > 1000000) {
       clearInterval(count);
-      downCount.textContent = "1M+"
+      downCount.textContent = "1M+";
     }
   }, 10);
 }
-const imageColor = document.querySelectorAll(".image")
-for (let  i = 0; i < imageColor.length ; i++){
-    imageColor[i].style.backgroundColor = `rgb(${Math.floor(Math.random() * 255)} ${Math.floor(Math.random() * 255)} ${Math.floor(Math.random() * 255)})`
+const imageColor = document.querySelectorAll(".image");
+for (let i = 0; i < imageColor.length; i++) {
+  imageColor[i].style.backgroundColor = `rgb(${Math.floor(
+    Math.random() * 255
+  )} ${Math.floor(Math.random() * 255)} ${Math.floor(Math.random() * 255)})`;
 }
 let cT = 0;
-const createEleBtn = document.querySelector(".image__plus")
-const imageEleParent = document.querySelector(".image__container")
-createEleBtn.addEventListener("click", function(e){
-  const imageEle = document.createElement("div")
-  imageEleParent.appendChild(imageEle)
-  imageEle.classList.add("image")
-  imageEle.style.backgroundColor = `rgb(${Math.floor(Math.random() * 255)} ${Math.floor(Math.random() * 255)} ${Math.floor(Math.random() * 255)})`
-  cT = Number(localStorage.getItem("IMAGE__NUMBER")) + 1
-  localStorage.setItem("IMAGE__NUMBER", cT)
-  console.log(cT)
-})
-imageEleParent.addEventListener("click", function(e){
-  const imageTargetToRemove = e.target
-  imageTargetToRemove.remove()
-  cT = Number(localStorage.getItem("IMAGE__NUMBER")) - 1
-  localStorage.setItem("IMAGE__NUMBER", cT)
-})
-function imageLoadingSystem(){
-  const imageEleParent = document.querySelector(".image__container")
-  for(let i = 0; i < localStorage.getItem("IMAGE__NUMBER"); i++){
-  const imageEle = document.createElement("div")
-  imageEleParent.appendChild(imageEle)
-  imageEle.classList.add("image")
-  imageEle.style.backgroundColor = `rgb(${Math.floor(Math.random() * 255)} ${Math.floor(Math.random() * 255)} ${Math.floor(Math.random() * 255)})`
+const createEleBtn = document.querySelector(".image__plus");
+const imageEleParent = document.querySelector(".image__container");
+createEleBtn.addEventListener("click", function (e) {
+  const imageEle = document.createElement("div");
+  const bannerEle = document.createElement("div");
+  const paraEle = document.createElement("p");
+  imageEleParent.appendChild(imageEle);
+  imageEle.classList.add("image");
+  imageEle.appendChild(bannerEle);
+  bannerEle.classList.add("banner");
+  bannerEle.appendChild(paraEle);
+  paraEle.addEventListener("click", function (e) {
+    e.stopPropagation();
+  });
+  paraEle.textContent = `${Math.floor(Math.random() * 10000)}`;
+  bannerEle.addEventListener("click", function (e) {
+    e.stopPropagation();
+  });
+  imageEle.style.backgroundColor = `rgb(${Math.floor(
+    Math.random() * 255
+  )} ${Math.floor(Math.random() * 255)} ${Math.floor(Math.random() * 255)})`;
+  cT = Number(localStorage.getItem("IMAGE__NUMBER")) + 1;
+  localStorage.setItem("IMAGE__NUMBER", cT);
+  console.log(cT);
+});
+
+imageEleParent.addEventListener("click", function (e) {
+  const imageTargetToRemove = e.target;
+  imageTargetToRemove.remove();
+  cT = Number(localStorage.getItem("IMAGE__NUMBER")) - 1;
+  localStorage.setItem("IMAGE__NUMBER", cT);
+});
+function imageLoadingSystem() {
+  const imageEleParent = document.querySelector(".image__container");
+  for (let i = 0; i < localStorage.getItem("IMAGE__NUMBER"); i++) {
+    const imageEle = document.createElement("div");
+    const bannerEle = document.createElement("div");
+    const paraEle = document.createElement("p");
+    imageEleParent.appendChild(imageEle);
+    imageEle.classList.add("image");
+    imageEle.appendChild(bannerEle);
+    bannerEle.classList.add("banner");
+    bannerEle.appendChild(paraEle);
+    paraEle.addEventListener("click", function (e) {
+      e.stopPropagation();
+    });
+    paraEle.textContent = `${Math.floor(Math.random() * 10000)}`;
+    bannerEle.addEventListener("click", function (e) {
+      e.stopPropagation();
+    });
+    imageEle.style.backgroundColor = `rgb(${Math.floor(
+      Math.random() * 255
+    )} ${Math.floor(Math.random() * 255)} ${Math.floor(Math.random() * 255)})`;
   }
 }
